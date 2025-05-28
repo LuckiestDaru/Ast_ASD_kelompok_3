cf7f262c0a15f784d01b2e7598808d332d2362d0
NatanaelWM <nathan.kudet@gmail.com>
2025-05-28 08:51:28 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct menuMakanan {
+    char namaMenu[50];
+    int harga;
+};
+struct menuMakanan menuMakan[100];
+
+struct strukHarga{
+    char namaMenu[50];
+    int jumlah;
+    int harga;
+};
+struct strukHarga strukPembelian[100];
+int maxIndexMenu, masIndexStruk;
+
+
+int main(){
+
+}
